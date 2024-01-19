4b4b53b71b04d696059b8088cc399ce72d3df7dc
年朔 <68497566+kexer2018@users.noreply.github.com>
2024-01-20 00:37:00 +0800

    feat: change async mothods and add schedule task

--- schedule/logger.task.js
+++ schedule/logger.task.js
@@ -1,7 +1,17 @@
 const { parentPort } = require('worker_threads')
 const path = require('path')
 const fs = require('fs')
+const {
+  readdir,
+  mkdir,
+  rename,
+  writeFile,
+  appendFile,
+  access,
+  unlink
+} = require('fs/promises')
 const moment = require('moment')
+const schedule = require('node-schedule')
 
 const dateFormat = 'yyyy-MM-DD HH:mm:ss'
 const MAX_SIZE = 10 * 1024 * 1024
@@ -12,14 +22,14 @@ let lastTime
 
 // 如果是 worker 线程，执行写入文件的任务
 if (parentPort) {
-  parentPort.on('message', (message) => {
+  parentPort.on('message', async message => {
     const log = joinMsg(message)
     const logMsg = formatLog(log)
     // print
     console.log(logMsg)
     // write
     const logFilePath = findLogFilePath(logMsg)
-    writeLogs(logFilePath, logMsg)
+    await writeLogs(logFilePath, logMsg)
   })
 }
 
@@ -27,25 +37,38 @@ if (parentPort) {
  *
  * @param {string} filePath
  * @param {string} msg
- * @returns
+ * @returns {Promise<void>}
  */
-function writeLogs(filePath, msg) {
+async function writeLogs(filePath, msg) {
   // 根据日志前的时间戳，找到对应的文件，如果没有则创建，有的话直接在后面接着写
-  if (!fs.existsSync(filePath)) {
-    fs.writeFileSync(filePath, msg + '\n')
+  try {
+    // 检查文件是否存在
+    try {
+      await access(filePath)
+    } catch (error) {
+      // 文件不存在，创建并写入
+      await writeFile(filePath, msg + '\n')
+      const { expiredFiles } = await selectFiles()
+      const expiredFilesPath = expiredFiles.map(item =>
+        path.join(logFolder, item)
+      )
+      await Promise.all(expiredFilesPath.map(item => changeFileName(item)))
+      return
+    }
 
-    const expiredFiles = selectFiles()
-    const expiredFilesPath = expiredFiles.map((item) =>
-      path.join(logFolder, item)
-    )
-    expiredFilesPath.forEach((item) => {
-      changeFileName(item)
-    })
-  } else {
-    fs.appendFileSync(filePath, msg + '\n')
+    // 文件存在，追加写入
+    await appendFile(filePath, msg + '\n')
+  } catch (error) {
+    console.error('Error writing logs:', error)
+    throw error // 抛出错误以便上层捕获
   }
 }
 
+/**
+ * 根据str获取写入的文件夹
+ * @param {string} str
+ * @returns
+ */
 function findLogFilePath(str) {
   const datePart = str.slice(1, 11)
   const hourPart = str.slice(12, 14)
@@ -84,8 +107,8 @@ function getLogFileMaxNum(timePrefix) {
   let maxNum = 0
   const logFilesInHour = fs
     .readdirSync(logFolder)
-    .filter((f) => f.startsWith(timePrefix) && f.endsWith('.log'))
-    .map((f) => {
+    .filter(f => f.startsWith(timePrefix) && f.endsWith('.log'))
+    .map(f => {
       const fileName = f.slice(0, -4) // 去掉文件后缀名
       const lastChar = fileName.slice(-1) // 提取字符串末尾的字符
       return parseInt(lastChar) // 将提取的字符转换为数字
@@ -125,43 +148,87 @@ function joinMsg(msgObj) {
 /**
  * 将文件名改变成过期文件
  */
-function changeFileName(filePath) {
+async function changeFileName(filePath) {
   const fileNameWithExtension = path.basename(filePath)
-  const newFileName = fileNameWithExtension.replace(
-    '.log',
-    `_invalid_${Date.now()}.log`
-  )
-  fs.renameSync(filePath, path.join(logFolder, newFileName))
+  let newFileName = ''
+  if (!filePath.includes('_invalid_')) {
+    newFileName = fileNameWithExtension.replace(
+      '.log',
+      `_invalid_${Date.now()}.log`
+    )
+  }
+  rename(filePath, path.join(logFolder, newFileName))
 }
 
 /**
  * 将文件移动到另一个trash文件夹
  */
-function moveFileToTrash(filePath) {
-  fs.renameSync(filePath, path.join(trashFolder, filePath))
+async function moveFileToTrash(filename) {
+  const originPath = path.join(logFolder, filename)
+  const trashDir = path.join(trashFolder, filename)
+  // 确保 trash 文件夹存在
+  await mkdir(trashFolder, { recursive: true })
+
+  await rename(originPath, trashDir)
+  console.log(`File moved to trash: ${filename}`)
 }
 
 /**
  *  从logs文件夹中筛选过期文件，将要移动的文件(不包含当前的日志文件)
  */
-
-function selectFiles() {
-  let expiredFiles = []
-  const files = fs.readdirSync(logFolder)
+async function selectFiles() {
   const currentHourPrefix = moment().format('YYYY-MM-DDTHH')
+  const files = await readdir(logFolder)
 
-  for (const file of files) {
-    if (!file.includes(currentHourPrefix)) {
-      expiredFiles.push(file)
-    }
-  }
-  return expiredFiles
+  const expiredFiles = files.filter(file => {
+    return !file.includes(currentHourPrefix) && !file.includes('_invalid_')
+  })
+
+  const needMoveFile = files.filter(file => file.includes('_invalid_'))
+
+  return { expiredFiles, needMoveFile }
 }
 
-function clearlogFiles() {
-  const files = fs.readdirSync(trashFolder)
-  for (const file of files) {
-    fs.unlinkSync(path.join(trashFolder, file))
-    console.log('Delete log Success!, filename:', file)
+/**
+ * 异步方式清空 trash 文件夹
+ * @returns {Promise<void>}
+ */
+async function clearlogFiles() {
+  try {
+    const files = await readdir(trashFolder);
+
+    // 使用 Promise.all 异步并行删除文件
+    await Promise.all(files.map(async (file) => {
+      await unlink(path.join(trashFolder, file));
+      console.log('Delete log Success!, filename:', file);
+    }));
+  } catch (error) {
+    console.error('Error clearing log files:', error);
   }
 }
+
+// 定时把日志文件移动到trash
+schedule.scheduleJob('0 * * * *', async () => {
+  try {
+    const files = await readdir(logFolder)
+    const { needMoveFile } = await selectFiles(files)
+    // 使用 Promise.all 异步并行处理文件
+    await Promise.all(
+      needMoveFile.map(async item => {
+        await moveFileToTrash(item)
+      })
+    )
+  } catch (error) {
+    console.error('Error in scheduled job:', error)
+  }
+})
+
+// 定时把日志文件删除
+schedule.scheduleJob('0 0 * * *', async () => {
+  try {
+    // 使用 Promise.all 异步并行处理文件
+    await clearlogFiles()
+  } catch (error) {
+    console.error('Error in scheduled job:', error)
+  }
+})
